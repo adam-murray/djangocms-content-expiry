19fa4873317ec679c38ec93691ea61892569a5db
bernard <bvdv9075@gmail.com>
2021-07-12 13:58:25 +0100

    Updated models, migration files and pep8/linting

--- djangocms_content_expiry/models.py
+++ djangocms_content_expiry/models.py
@@ -1,9 +1,19 @@
+import datetime
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+# Temporary placeholder
+TYPE_CHOICES = ("Page", "Alias", )
+
 
 class ContentExpiry(models.Model):
-    label = models.CharField(verbose_name=_('label'), max_length=100)
+    type = models.CharField(
+        max_length=50,
+        choices=TYPE_CHOICES
+    )
+    from_expiry_date = models.DateField(_("From Expiry Date"), default=datetime.date.today)
+    to_expiry_date = models.DateField(_("To Expiry Date"), default=datetime.date.today)
 
     class Meta:
         verbose_name = _("Content Expiry")
