8cd9d9d0eb44181881a66fe649797aa39649db74
bernard <bvdv9075@gmail.com>
2021-07-13 09:57:27 +0100

    Updated models with versioning tables

--- djangocms_content_expiry/models.py
+++ djangocms_content_expiry/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 # Temporary placeholder
-TYPE_CHOICES = ("Page", "Alias", )
+TYPE_CHOICES = (("Page", "Page"), ("Alias", "Alias"),)
 
 
 class ContentExpiry(models.Model):
@@ -12,6 +12,10 @@ class ContentExpiry(models.Model):
         max_length=50,
         choices=TYPE_CHOICES
     )
+
+
+class ContentExpiryContent(models.Model):
+    page_type = models.ForeignKey(ContentExpiry, on_delete=models.CASCADE)
     from_expiry_date = models.DateField(_("From Expiry Date"), default=datetime.date.today)
     to_expiry_date = models.DateField(_("To Expiry Date"), default=datetime.date.today)
 
