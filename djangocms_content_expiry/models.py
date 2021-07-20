e34b68ff53ef69ad323c97d69e2da3968cffa264
bernard <bvdv9075@gmail.com>
2021-07-20 13:36:13 +0100

    testing workflow

--- djangocms_content_expiry/models.py
+++ djangocms_content_expiry/models.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-# Temporary placeholder
+# Temporary placeholders
 TYPE_CHOICES = (("Page", "Page"), ("Alias", "Alias"),)
 
 
