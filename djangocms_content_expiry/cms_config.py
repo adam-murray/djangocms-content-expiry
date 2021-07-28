d84b81decd569cf1f51fbdf22f7f374faaf3c015
bernard <bvdv9075@gmail.com>
2021-07-28 13:33:46 +0100

    App config entries

--- djangocms_content_expiry/cms_config.py
+++ djangocms_content_expiry/cms_config.py
@@ -1,26 +1,50 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
-from cms.app_base import CMSAppConfig
+from cms.app_base import CMSAppConfig, CMSAppExtension
+from cms.models import Page
 from cms.utils.i18n import get_language_tuple
 
 from .models import ContentExpiryContent
 
 
+class ContentExpiryCMSExtension(CMSAppExtension):
+    def __init__(self):
+        self.content_expiry_apps_models = {}
+
+    def configure_app(self, cms_config):
+        if hasattr(cms_config, "content_expiry_models"):
+            content_expiry_apps_models = getattr(cms_config, "content_expiry_models")
+            if isinstance(content_expiry_apps_models, dict):
+                self.content_expiry_apps_models.update(content_expiry_apps_models)
+            else:
+                raise ImproperlyConfigured(
+                    "navigation configuration must be a dictionary object"
+                )
+        else:
+            raise ImproperlyConfigured(
+                "cms_config.py must have navigation_models attribute"
+            )
+
+
 class ContentExpiryAppConfig(CMSAppConfig):
+    djangocms_content_expiry_enabled = True
     djangocms_versioning_enabled = getattr(
         settings, "DJANGOCMS_NAVIGATION_VERSIONING_ENABLED", True
     )
     djangocms_moderation_enabled = getattr(
         settings, "DJANGOCMS_NAVIGATION_MODERATION_ENABLED", True
     )
-
+    content_expiry_models = {
+        Page: ["title"]
+    }
     if djangocms_versioning_enabled:
         from djangocms_versioning.datastructures import VersionableItem, default_copy
 
         versioning = [
             VersionableItem(
                 content_model=ContentExpiryContent,
-                grouper_field_name="page_type",
+                grouper_field_name="content_type",
                 version_list_filter_lookups={"language": get_language_tuple},
                 copy_function=default_copy
             )
