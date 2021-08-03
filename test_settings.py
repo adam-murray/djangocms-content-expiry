d67ab88484bec4a6717b238b90d23ef371cb6479
bernard <bvdv9075@gmail.com>
2021-08-03 15:16:06 +0100

    removed versioning from installed apps in test_settings.py

--- test_settings.py
+++ test_settings.py
@@ -1,7 +1,6 @@
 HELPER_SETTINGS = {
     "TIME_ZONE": "America/Chicago",
     "INSTALLED_APPS": [
-        "djangocms_versioning",
         "djangocms_content_expiry",
     ],
     "MIGRATION_MODULES": {
@@ -9,7 +8,6 @@ HELPER_SETTINGS = {
         "cms": None,
         "menus": None,
         "djangocms_content_expiry": None,
-        "djangocms_versioning": None,
     },
     "CMS_PERMISSION": True,
     "LANGUAGES": (
