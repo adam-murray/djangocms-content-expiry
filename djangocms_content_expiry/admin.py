d54905e7ce2fad9270723a92a2debc24b37aeca4
bernard <bvdv9075@gmail.com>
2021-08-05 14:32:42 +0100

    linting

--- djangocms_content_expiry/admin.py
+++ djangocms_content_expiry/admin.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from django.conf.urls import url
-from django.utils.translation import ugettext, ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 from djangocms_content_expiry.models import ContentExpiry
-from djangocms_versioning.admin import ExtendedVersionAdminMixin
-
-from .views import PreviewView
+# from djangocms_versioning.admin import ExtendedVersionAdminMixin
 
 
 @admin.register(ContentExpiry)
-class ContentExpiryAdmin(ExtendedVersionAdminMixin, admin.ModelAdmin):
+class ContentExpiryAdmin(admin.ModelAdmin):
     list_display = ['content_type', 'from_expiry_date', 'to_expiry_date']
     fieldsets = (
             (None, {
@@ -19,13 +16,3 @@ class ContentExpiryAdmin(ExtendedVersionAdminMixin, admin.ModelAdmin):
 
     verbose_name = _("Content")
     verbose_name_plural = _("Contents")
-
-    def get_urls(self):
-        info = self.model._meta.app_label, self.model._meta.model_name
-        return [
-            url(
-                r"^(?P<id>\d+)/preview/$",
-                self.admin_site.admin_view(PreviewView.as_view()),
-                name="{}_{}_preview".format(*info),
-            )
-        ] + super().get_urls()
