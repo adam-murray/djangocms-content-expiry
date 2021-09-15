9d671d3590a7c8bdda03aa462c44e7b793664761
bernard <bvdv9075@gmail.com>
2021-09-15 11:27:58 +0100

    Moved functions from helpers to models

--- djangocms_content_expiry/helpers.py
+++ djangocms_content_expiry/helpers.py
@@ -1,9 +1,5 @@
 from django.contrib.auth import get_user_model
 
-from djangocms_content_expiry.models import ContentExpiry
-
-from dateutil.relativedelta import relativedelta
-
 
 def get_authors():
     """
@@ -11,43 +7,3 @@ def get_authors():
     """
     User = get_user_model()
     return User.objects.filter(contentexpiry__created_by__isnull=False).distinct()
-
-
-def version_has_expiry(version):
-    """
-    Determine if a version has expiry record added
-    """
-    return getattr(version, "contentexpiry", None)
-
-
-def create_version_expiry(version):
-    """
-    Helper to create a new entry in versioning with the expiration date
-    """
-    ContentExpiry.objects.get_or_create(
-        version=version,
-        created=version.created,
-        created_by=version.created_by,
-        expires=_get_future_expire_date(version.modified)
-    )
-    return version
-
-
-def copy_content_expiry(old_record, new_version):
-    """
-    Create a content expiry record with existing expiry date
-    """
-    version = ContentExpiry.objects.get_or_create(
-        version=new_version,
-        created=new_version.created,
-        created_by=new_version.created_by,
-        expires=old_record[0].expires
-    )
-    return version
-
-
-def _get_future_expire_date(modified_date):
-    """
-     Expire date should be published + one year
-    """
-    return modified_date + relativedelta(years=1)
--- djangocms_content_expiry/monkeypatch/models.py
+++ djangocms_content_expiry/monkeypatch/models.py
@@ -1,11 +1,8 @@
 from djangocms_versioning import models
 
-from djangocms_content_expiry.helpers import copy_content_expiry, create_version_expiry, version_has_expiry
 from djangocms_content_expiry.models import ContentExpiry
 
-from djangocms_versioning.constants import DRAFT, PUBLISHED
-from djangocms_versioning.models import Version
-from djangocms_versioning import versionables
+from dateutil.relativedelta import relativedelta
 
 
 def new_save(old_save):
@@ -16,17 +13,57 @@ def new_save(old_save):
         old_save(version, **kwargs)
 
         # Check if version has content expiry added
-        new_version = version_has_expiry(version)
+        new_version = _version_has_expiry(version)
 
         if new_version is not None:
             # If version does not have an expiry record, it needs to be created
-            create_version_expiry(version)
+            _create_version_expiry(version)
         else:
             # Content expiry object exists so copying the original expiry object
             old_expiry_record = ContentExpiry.objects.filter(version_id=version.source_id)
-            copy_content_expiry(old_expiry_record, version)
+            _copy_content_expiry(old_expiry_record, version)
         return version
     return inner
 
 
 models.Version.save = new_save(models.Version.save)
+
+
+def _copy_content_expiry(old_record, new_version):
+    """
+    Create a content expiry record with existing expiry date
+    """
+    version = ContentExpiry.objects.get_or_create(
+        version=new_version,
+        created=new_version.created,
+        created_by=new_version.created_by,
+        expires=old_record[0].expires
+    )
+    return version
+
+
+def _create_version_expiry(version):
+    """
+    Helper to create a new entry in versioning with the expiration date
+    """
+    ContentExpiry.objects.get_or_create(
+        version=version,
+        created=version.created,
+        created_by=version.created_by,
+        expires=_get_future_expire_date(version.modified)
+    )
+    return version
+
+
+def _get_future_expire_date(modified_date):
+    """
+     Expire date should be published + one year
+    """
+    return modified_date + relativedelta(years=1)
+
+
+def _version_has_expiry(version):
+    """
+    Determine if a version has expiry record added
+    """
+    return getattr(version, "contentexpiry", None)
