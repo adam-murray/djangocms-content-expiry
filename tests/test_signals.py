938051f75e155d24eff58be3d40c955f86c74268
bernard <bvdv9075@gmail.com>
2021-09-17 20:23:51 +0100

    Updated signal test to see if expiry record is created with new version

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,25 +1,27 @@
 from cms.test_utils.testcases import CMSTestCase
-from cms.test_utils.util.context_managers import signal_tester
 
-import datetime
-from unittest import skip
+from cms.api import create_page
 
-from djangocms_versioning.constants import DRAFT
+from djangocms_versioning.models import Version
 
-from djangocms_content_expiry.test_utils.polls.factories import PollContentExpiryFactory
-
-from djangocms_versioning import signals
+from djangocms_content_expiry.models import ContentExpiry
 
 
 class ContentExpirySignalTestCase(CMSTestCase):
-    # TODO: Finish the test
-    @skip("Test not complete")
-    def test_submitted_for_review_signal(self):
+    def test_content_expiry_creation_signal(self):
         """
-        Test that creating a new version emits a signal
+        Test that creating a new version emits the signal to create a content expiry record
         """
-        delta = datetime.timedelta(days=31)
-        expire = datetime.datetime.now() + delta
-        PollContentExpiryFactory(expires=expire, version__state=DRAFT)
-        with signal_tester(signals.post_version_operation) as env:
-            self.assertEqual(env.call_count, 1)
+        language = "en"
+        user = self.get_superuser()
+        page = create_page(
+            title="home",
+            template="page.html",
+            language=language,
+            created_by=user
+        )
+
+        version = Version.objects.filter_by_grouper(page)
+        expiry_record = ContentExpiry.objects.filter(version=version[0])
+        self.assertEqual(len(expiry_record), 1)
+        self.assertTrue(hasattr(expiry_record[0], "expires"))
