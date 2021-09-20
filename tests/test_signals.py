8becbc4b6ee8aa22629edd8acda27c3457ab96ac
bernard <bvdv9075@gmail.com>
2021-09-20 10:12:57 +0100

    Added test to ensure signal is triggered and only on draft

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -1,7 +1,10 @@
 from cms.api import create_page
 from cms.test_utils.testcases import CMSTestCase
+from cms.test_utils.util.context_managers import signal_tester
 
+from djangocms_versioning import constants
 from djangocms_versioning.models import Version
+from djangocms_versioning import signals
 
 from djangocms_content_expiry.models import ContentExpiry
 
@@ -9,7 +12,7 @@ from djangocms_content_expiry.models import ContentExpiry
 class ContentExpirySignalTestCase(CMSTestCase):
     def test_content_expiry_creation_signal(self):
         """
-        Test that creating a new version emits the signal to create a content expiry record
+        Test content expiry exists in the new version
         """
         language = "en"
         user = self.get_superuser()
@@ -24,3 +27,26 @@ class ContentExpirySignalTestCase(CMSTestCase):
         expiry_record = ContentExpiry.objects.filter(version=version[0])
         self.assertEqual(len(expiry_record), 1)
         self.assertTrue(hasattr(expiry_record[0], "expires"))
+
+    def test_submitted_for_review_signal(self):
+        """
+        Test that creating a new page emits 1 signal to create content expiry and only executes on creating a draft
+        """
+        with signal_tester(signals.post_version_operation) as env:
+            language = "en"
+            user = self.get_superuser()
+            create_page(
+                title="home",
+                template="page.html",
+                language=language,
+                created_by=user
+            )
+
+            self.assertEqual(env.call_count, 1)
+            signal = env.calls[0][1]
+            self.assertEqual(signal["operation"], constants.OPERATION_DRAFT)
+
+
+
+
+
