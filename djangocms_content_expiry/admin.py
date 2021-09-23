5d6f1a54256fe4b034f5277a4d97fcc4c2a6675f
bernard <bvdv9075@gmail.com>
2021-09-23 16:30:46 +0100

    cleanup and comments

--- djangocms_content_expiry/admin.py
+++ djangocms_content_expiry/admin.py
@@ -85,6 +85,10 @@ class ContentExpiryAdmin(admin.ModelAdmin):
         ] + super().get_urls()
 
     def export_to_csv(self, request):
+        """
+        Retrieves queryset
+        Exports the retrieved queryset to csv format
+        """
         queryset = self.get_export_queryset(request)
         meta = self.model._meta
         field_names = ['Title', 'Content Type', 'Expiry Date', 'Version State', 'Version Author']
@@ -114,13 +118,21 @@ class ContentExpiryAdmin(admin.ModelAdmin):
         list_filter = self.get_list_filter(request)
         search_fields = self.get_search_fields(request)
 
-        ChangeList = self.get_changelist(request)
-        changelist_kwargs = {'request': request, 'model': self.model, 'list_display': list_display,
-                             'list_display_links': list_display_links, 'list_filter': list_filter,
-                             'date_hierarchy': self.date_hierarchy, 'search_fields': search_fields,
-                             'list_select_related': self.list_select_related, 'list_per_page': self.list_per_page,
-                             'list_max_show_all': self.list_max_show_all, 'list_editable': self.list_editable,
-                             'model_admin': self, 'sortable_by': self.sortable_by}
-        cl = ChangeList(**changelist_kwargs)
+        changelist = self.get_changelist(request)
+        changelist_kwargs = {'request': request,
+                             'model': self.model,
+                             'list_display': list_display,
+                             'list_display_links': list_display_links,
+                             'list_filter': list_filter,
+                             'date_hierarchy': self.date_hierarchy,
+                             'search_fields': search_fields,
+                             'list_select_related': self.list_select_related,
+                             'list_per_page': self.list_per_page,
+                             'list_max_show_all': self.list_max_show_all,
+                             'list_editable': self.list_editable,
+                             'model_admin': self,
+                             'sortable_by': self.sortable_by
+                             }
+        cl = changelist(**changelist_kwargs)
 
         return cl.get_queryset(request)
